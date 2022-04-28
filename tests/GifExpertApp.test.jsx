20f4522f4dd3ba6d42faf5a3aa9be8e3a1cdd497
Fernando Herrera <fernando.herrera85@gmail.com>
2022-04-28 19:20:00 -0400

    Fin sección 8

--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -1,9 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import { GifExpertApp } from '../src/GifExpertApp';
 
 
 describe('Pruebas en <GifExpertApp />', () => {
 
     test('should ', () => {
         
+        render( <GifExpertApp /> );
+        screen.debug();
+
+
     });
 
 
